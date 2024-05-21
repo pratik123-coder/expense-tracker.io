21780e66e868d666f3f10f8fe4246b0a69f4e929
pratik123-coder <pratikmohanty939@yahoo.com>
2024-05-21 23:13:35 +0530

    Frontend Setup || Vite Configured || ShadCn Setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
-
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
+    'index.html',
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -75,6 +75,4 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
-
-export default config
+}
